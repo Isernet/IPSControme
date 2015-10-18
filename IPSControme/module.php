1a26d623f177232a46cbac4d2a958e6bd35e7646
Isernet <Jens@MacBook-Air.fritz.box>
2015-10-18 15:25:16 +0200

    Namen angepasst

--- IPSControme/module.php
+++ IPSControme/module.php
@@ -1,6 +1,6 @@
 <?
     // Klassendefinition
-    class Controme extends IPSModule {
+    class IPSControme extends IPSModule {
         /**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
         * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
