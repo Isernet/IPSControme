528630c1d6379759e743606f8e881e7cf098635e
Isernet <Jens@MacBook-Air.fritz.box>
2015-10-18 13:20:44 +0200

    Umbau auf SDK_Beispiel

--- Controme/module.php
+++ Controme/module.php
@@ -5,7 +5,7 @@
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
         * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
         *
-        * ABC_MeineErsteEigeneFunktion($id);
+        * CON_MeineErsteEigeneFunktion($id);
         *
         */
         public function MeineErsteEigeneFunktion() {
